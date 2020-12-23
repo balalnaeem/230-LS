129b6f98c1f828ef62a504dfd9561acedd1af6d6
Balal Naeem <balalnaeem@gmail.com>
2020-12-23 14:12:54 +0000

    practice problems

---
+++ revision_3/exercises/gui_apps/js_wysiwyg/script.js
@@ -0,0 +1,9 @@
+const $menu = $('#menu-bar');
+
+function toggleBold(e) {
+  e.preventDefault();
+  document.execCommand('bold', false, null);
+}
+
+$('button').on('click', toggleBold);
+$('i.fa-bold').on('click', toggleBold);
